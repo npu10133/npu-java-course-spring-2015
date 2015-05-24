f151256e7c1dd18ae687e3ad3645868c6aebba93
npu10133 <houandy2@gmail.com>
2015-05-24 17:12:00 +0800

    v.3

--- ObserverSample/src/tw/edu/npu/mis/Controller.java
+++ ObserverSample/src/tw/edu/npu/mis/Controller.java
@@ -2,7 +2,11 @@
 package tw.edu.npu.mis;
 
 import java.util.Scanner;
-
+/**
+ * {@link Controller} retrieves inputs from the user.
+ *
+ * @author Samael Wang <freesamael@gmail.com>
+ */
 public class Controller {
 
     private final Model mModel;
@@ -10,7 +14,9 @@ public class Controller {
     public Controller(Model model) {
         mModel = model;
     }
-
+ /**
+     * Read input and update model accordingly.
+     */
     public void readInput() {
         System.out.print("Controller: ");
         Scanner s = new Scanner(System.in);
--- ObserverSample/src/tw/edu/npu/mis/Window.java
+++ ObserverSample/src/tw/edu/npu/mis/Window.java
@@ -3,13 +3,22 @@ package tw.edu.npu.mis;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Simulate Window objects in GUI toolkits.
+ *
+ * @author Samael Wang <freesamael@gmail.com>
+ */
 
 public class Window {
 
     private Controller mController;
     private List<View> mInvalidViews;
-
+ /**
+     * Start the event loop.
+     *
+     * @param c The controller.
+     * @param views The views to draw on the first loop.
+     */
   
     public void startEventLoop(Controller c, List<View> views) {
         mController = c;
@@ -24,7 +33,11 @@ public class Window {
             //mInvalidViews.clear();
         }
     }
-
+  /**
+     * Add a view to a queue for redraw on screen later.
+     *
+     * @param v View to redraw.
+     */
     
     public void schduleRedraw(View v) {
         mInvalidViews.add(v);
